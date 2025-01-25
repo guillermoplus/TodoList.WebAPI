1dc9acfd696ea281164f1cd172e19ef70b32cca7
guillermoplus <luisgal93@hotmail.com>
2025-01-25 16:49:34 -0500

    add JWT authentication; implement AuthController and JwtService; update appsettings for JWT configuration

--- TodoList.WebAPI/Controllers/TodoTaskController.cs
+++ TodoList.WebAPI/Controllers/TodoTaskController.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using TodoList.Application.DTO;
 using TodoList.Application.UseCases;
@@ -6,6 +7,7 @@ namespace TodoList.WebAPI.Controllers;
 
 [ApiController]
 [Route("api/[controller]")]
+[Authorize]
 public class TodoTaskController(
     ILogger<TodoTaskController> logger,
     GetTodoTaskByIdUseCase getTodoTaskByIdUseCase,
--- TodoList.WebAPI/Program.cs
+++ TodoList.WebAPI/Program.cs
@@ -1,12 +1,44 @@
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using TodoList.Application.UseCases;
 using TodoList.Domain.Interfaces;
 using TodoList.Infrastructure.Persistence;
 using TodoList.Infrastructure.Repositories;
+using TodoList.WebAPI.Interfaces;
+using TodoList.WebAPI.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+// Jwt
+var jwtSettings = builder.Configuration.GetSection("JwtSettings");
+var key = jwtSettings.GetValue<string>("Secret");
+
+builder.Services.AddAuthentication(opt =>
+{
+    opt.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    opt.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(opt =>
+{
+    opt.TokenValidationParameters = new TokenValidationParameters()
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = jwtSettings.GetValue<string>("Issuer"),
+        ValidAudience = jwtSettings.GetValue<string>("Audience"),
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(key))
+    };
+});
+
+builder.Services.AddAuthorization();
+
+builder.Services.AddSingleton<IJwtService, JwtService>();
 
 // Database connection
 builder.Services.AddDbContext<TodoListDbContext>(opt =>
@@ -24,7 +56,33 @@ builder.Services.AddScoped<MarkTaskAsCompletedUseCase>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(opt =>
+{
+    opt.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        Description = "Header de autorización JWT utilizando el esquema Bearer. Ejemplo: \"Bearer {token}\"",
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.Http
+    });
+
+    opt.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            Array.Empty<string>()
+        }
+    });
+});
 
 var app = builder.Build();
 
@@ -37,7 +95,10 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
+
 app.UseAuthorization();
+
 
 app.MapControllers();
 
